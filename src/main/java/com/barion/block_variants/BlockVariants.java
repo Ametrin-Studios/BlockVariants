8023140c962b3d7b7e4e08e7dab19c3d6b440ae2
Barion <ametrin.studios@gmail.com>
2023-12-23 00:21:42 +0100

    using new DataProviderHelper

--- src/main/java/com/barion/block_variants/BlockVariants.java
+++ src/main/java/com/barion/block_variants/BlockVariants.java
@@ -1,20 +1,18 @@
 package com.barion.block_variants;
 
-import com.ametrinstudios.ametrin.data.provider.CustomLootTableProvider;
+import com.ametrinstudios.ametrin.data.DataProviderHelper;
 import com.barion.block_variants.data.provider.*;
 import com.barion.block_variants.data.provider.loot_table.BVBlockLootSubProvider;
-import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import org.slf4j.Logger;
 
 @Mod(BlockVariants.MOD_ID)
 public class BlockVariants{
     public static final String MOD_ID = "block_variants";
-    public static final Logger Logger = LogUtils.getLogger();
+//    public static final Logger Logger = LogUtils.getLogger();
 
     public BlockVariants(IEventBus modBus) {
         BVBlocks.BLOCK_REGISTER.register(modBus);
@@ -29,21 +27,12 @@ public class BlockVariants{
     }
 
     private static void gatherData(GatherDataEvent event){
-        var generator = event.getGenerator();
-        var output = generator.getPackOutput();
-        var existingFileHelper = event.getExistingFileHelper();
-        var lookup = event.getLookupProvider();
+        var providers = new DataProviderHelper(event);
 
-        var runClient = event.includeClient();
-        var runServer = event.includeServer();
-
-        generator.addProvider(runServer, new BVBlockStateProvider(output, existingFileHelper));
-        generator.addProvider(runServer, new BVItemModelProvider(output, existingFileHelper));
-        BVBlockTagsProvider blockTags = new BVBlockTagsProvider(output, lookup, existingFileHelper);
-        generator.addProvider(runServer, blockTags);
-        generator.addProvider(runServer, new BVItemTagsProvider(output, lookup, blockTags.contentsGetter(), existingFileHelper));
-        generator.addProvider(runServer, new BVRecipeProvider(output, lookup));
-        var lootTableProvider = CustomLootTableProvider.Builder().AddBlockProvider(BVBlockLootSubProvider::new).Build(output);
-        generator.addProvider(runServer, lootTableProvider);
+        providers.add(BVBlockStateProvider::new);
+        providers.add(BVItemModelProvider::new);
+        providers.add(BVRecipeProvider::new);
+        providers.addBlockAndItemTags(BVBlockTagsProvider::new, BVItemTagsProvider::new);
+        providers.addLootTables(builder -> builder.AddBlockProvider(BVBlockLootSubProvider::new));
     }
 }
