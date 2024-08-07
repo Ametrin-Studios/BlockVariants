011af0f078e5048c2d3cab3201ae1f651c3ddcd0
Barion <ametrin.studios@gmail.com>
2024-08-07 21:04:31 +0200

    re-added ametrin as dependency

--- src/main/java/com/ametrinstudios/ametrin/data/BlockTagProviderRule.java
+++
@@ -1,8 +0,0 @@
-package com.ametrinstudios.ametrin.data;
-
-import net.minecraft.world.level.block.Block;
-
-@FunctionalInterface
-public interface BlockTagProviderRule {
-    void generate(Block block, String name);
-}
