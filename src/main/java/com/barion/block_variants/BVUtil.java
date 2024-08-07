2988692367321eaaeb7ec1d8d04cf0d298479110
Barion <ametrin.studios@gmail.com>
2024-08-07 21:06:57 +0200

    renamed tag `c:wooden_wall` to `c:wooden_walls` added tag translations

--- src/main/java/com/barion/block_variants/BVUtil.java
+++ src/main/java/com/barion/block_variants/BVUtil.java
@@ -8,12 +8,11 @@ import net.minecraft.world.level.block.Block;
 import java.util.Objects;
 
 public final class BVUtil {
-    public static boolean isBasalt(String name) {return (name.contains("basalt") && !name.contains("smooth"));}
-    public static boolean shouldAppendBlock(String name) {return (name.contains("purpur"));}
-    public static boolean isCutSandstone(String name) {return (name.contains("cut") && name.contains("sandstone"));}
+    public static boolean isBasalt(String name) { return (name.contains("basalt") && !name.contains("smooth")); }
+    public static boolean shouldAppendBlock(String name) { return (name.contains("purpur")); }
+    public static boolean isCutSandstone(String name) { return (name.contains("cut") && name.contains("sandstone")); }
 
-    public static String getBlockName(Block block) {return getBlockID(block).getPath();}
+    public static String getBlockName(Block block) { return getBlockID(block).getPath(); }
     public static ResourceLocation getBlockID(Block block) { return Objects.requireNonNull(BuiltInRegistries.BLOCK.getKey(block)); }
-
     public static ResourceLocation getItemID(Item block) { return Objects.requireNonNull(BuiltInRegistries.ITEM.getKey(block)); }
 }
--- src/main/java/com/barion/block_variants/BlockVariants.java
+++ src/main/java/com/barion/block_variants/BlockVariants.java
@@ -3,6 +3,7 @@ package com.barion.block_variants;
 import com.ametrinstudios.ametrin.data.DataProviderHelper;
 import com.barion.block_variants.data.provider.*;
 import com.barion.block_variants.data.provider.loot_table.BVBlockLootSubProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -21,18 +22,22 @@ public final class BlockVariants{
         modBus.addListener(BlockVariants::gatherData);
     }
 
+    public static ResourceLocation locate(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+    }
+
     private static void buildCreativeModeTabs(BuildCreativeModeTabContentsEvent event){
         if(event.getTabKey() != CreativeModeTabs.BUILDING_BLOCKS) return;
         BVBlocks.BLOCK_REGISTER.getEntries().forEach((blockHolder)-> event.accept(blockHolder.get()));
     }
 
     private static void gatherData(GatherDataEvent event){
-        var providers = new DataProviderHelper(event);
+        var helper = new DataProviderHelper(event);
 
-        providers.add(BVBlockStateProvider::new);
-        providers.add(BVItemModelProvider::new);
-        providers.add(BVRecipeProvider::new);
-        providers.addBlockAndItemTags(BVBlockTagsProvider::new, BVItemTagsProvider::new);
-        providers.addLootTables(builder -> builder.AddBlockProvider(BVBlockLootSubProvider::new));
+        helper.add(BVBlockStateProvider::new);
+        helper.add(BVItemModelProvider::new);
+        helper.add(BVRecipeProvider::new);
+        helper.addBlockAndItemTags(BVBlockTagsProvider::new, BVItemTagsProvider::new);
+        helper.addLootTables(builder -> builder.addBlockProvider(BVBlockLootSubProvider::new));
     }
 }
